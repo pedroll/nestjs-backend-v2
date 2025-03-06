7ea19d3bcd2b56f0658739fb4bd5dab109b6699f
Pedro LLongo <pedrollongo@gmail.com>
2025-03-06 17:16:15 +0100

    feat: enhance CreateProductDto with validation and update product API endpoints

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,44 @@
-export class CreateProductDto {}
+import {
+  IsArray,
+  IsIn,
+  IsNumber,
+  IsOptional,
+  IsPositive,
+  IsString,
+  MinLength,
+} from 'class-validator';
+import { Type } from 'class-transformer';
+
+export class CreateProductDto {
+  @IsString()
+  @MinLength(3)
+  name: string;
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  @Type(() => Number)
+  price?: number;
+
+  @IsOptional()
+  @IsString()
+  description?: string;
+
+  @IsOptional()
+  @IsString()
+  slug?: string;
+
+  @IsOptional()
+  @IsNumber()
+  @IsPositive()
+  @Type(() => Number)
+  stock?: number;
+
+  @IsArray()
+  @IsString({ each: true })
+  sizes: string[];
+
+  @IsIn(['man', 'woman', 'kids', 'unisex'])
+  @IsString({ each: true })
+  gender?: string;
+}
