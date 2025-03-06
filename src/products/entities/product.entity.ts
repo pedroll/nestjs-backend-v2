f24bca3bade7b50970a8c36e86d619546292c5de
Pedro LLongo <pedrollongo@gmail.com>
2025-03-06 16:26:12 +0100

    - simple entity sin relaciones

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -2,9 +2,49 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class Product {
+  // Usamos PostgreSQL, por lo tanto, los tipos de columna deben ser acordes
   @PrimaryGeneratedColumn('uuid')
-  id: number;
+  id: string;
 
-  @Column('text', { unique: true })
+  @Column('text', {
+    unique: true,
+  })
   name: string;
+
+  @Column('numeric', {
+    default: 0,
+  })
+  price: number;
+
+  // despription es opcional
+  @Column({
+    type: 'text',
+    nullable: true,
+  })
+  description: string;
+
+  //eslug es opcional
+  @Column('text', {
+    unique: true,
+  })
+  slug: string;
+
+  // stock es opcional
+  @Column('numeric', {
+    default: 0,
+  })
+  stock: number;
+
+  //sizes []
+  @Column('text', {
+    array: true,
+    nullable: true,
+  })
+  sizes: string[];
+
+  // gender es opcional
+  @Column('text', {
+    nullable: true,
+  })
+  gender: string;
 }
