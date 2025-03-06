0d1f2fd30c14a9216ee04f54b0d3e8f2a6e77f71
Pedro LLongo <pedrollongo@gmail.com>
2025-03-06 18:50:07 +0100

    feat: improve error handling in ProductsService and enhance logging

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,18 +2,27 @@ import {
   BadRequestException,
   Injectable,
   InternalServerErrorException,
+  Logger,
+  NotFoundException,
 } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { InjectRepository } from '@nestjs/typeorm';
 
+import {
+  CannotCreateEntityIdMapError,
+  EntityNotFoundError,
+  QueryFailedError,
+  Repository,
+} from 'typeorm';
+
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { Repository } from 'typeorm';
 import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
   private paginationLimit;
+  private readonly logger = new Logger(ProductsService.name);
 
   constructor(
     @InjectRepository(Product)
@@ -25,17 +34,15 @@ export class ProductsService {
 
   async create(createProductDto: CreateProductDto) {
     try {
-      // Convert the Product name to lowercase
-      //createProductDto.name = createProductDto.name.toLocaleLowerCase();
-
-      // Create a new Pokémon instance
+      // Create a new Product instance
       const newProduct = this.productRepository.create(createProductDto);
+      // Save the new Product to the database
+      await this.productRepository.save(newProduct);
 
-      // Save the new Pokémon to the database
-      return await this.productRepository.save(newProduct);
+      return newProduct;
     } catch (error) {
       // Handle any exceptions that occur during the creation process
-      this.handleException(error);
+      this.handleDbException(error);
     }
   }
 
@@ -59,17 +66,25 @@ export class ProductsService {
    * Handles exceptions that occur during database operations.
    *
    * @param error - The error object.
-   * @throws BadRequestException if a Pokémon with the same name or "no" already exists.
+   * @throws BadRequestException if a Product with the same name or "no" already exists.
    * @throws InternalServerErrorException for other errors.
    */
-  private handleException(error) {
-    if (error.code === 11000) {
-      throw new BadRequestException(
-        `Pokemon already exists ${JSON.stringify(error.keyValue)}`,
-      );
+  private handleDbException(error) {
+    if (error instanceof QueryFailedError) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      if (error.driverError.code === '23505') {
+        throw new BadRequestException(
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+          `Product already exists ${JSON.stringify(error.driverError.detail)}`,
+        );
+      }
+    } else if (error instanceof EntityNotFoundError) {
+      throw new NotFoundException(`Product not found`);
+    } else if (error instanceof CannotCreateEntityIdMapError) {
+      throw new InternalServerErrorException(`Failed to create entity ID map`);
     }
 
-    console.error(error);
+    this.logger.error(error);
     throw new InternalServerErrorException(
       `Failed to create Product: consult logs`,
     );
