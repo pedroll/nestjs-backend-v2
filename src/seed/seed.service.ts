3e4ac142a4223fc185c86ba29611cf4dafa39ea2
Pedro LLongo <pedrollongo@gmail.com>
2025-03-14 23:28:29 +0100

    Refactor seed service to use async/await and improve product insertion

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,18 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { ProductsService } from '../products/products.service';
+import { initialData } from './data/seed-data';
+import { CreateProductDto } from '../products/dto/create-product.dto';
+import { Product } from '../products/entities/product.entity';
 
 @Injectable()
 export class SeedService {
   constructor(private readonly productsService: ProductsService) {}
-  runSeed(): any {
-    this.insertNewProducts();
+
+  async runSeed(): Promise<any> {
+    await this.insertNewProducts();
     return { message: 'Seed executed' };
   }
 
   private async insertNewProducts() {
     await this.productsService.removeAllProducts();
-    // insert products
 
-    // Create products
+    const products = initialData.products;
+    const inserPromises: any[] = [];
+
+    products.forEach((product) => {
+      inserPromises.push(this.productsService.create(product));
+    });
+
+    await Promise.all(inserPromises);
+
+    return { message: 'Products inserted' };
   }
 }
