e260c2aed0180cff02dfee89140fd4ea99ed2b11
Pedro LLongo <pedrollongo@gmail.com>
2025-03-18 00:35:49 +0100

    Add functionality to retrieve product images by filename

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,9 +1,13 @@
 import {
   Controller,
+  Get,
+  Param,
   Post,
+  Res,
   UploadedFile,
   UseInterceptors,
 } from '@nestjs/common';
+import { Response } from 'express'; // Import Response from express
 import { FilesService } from './files.service';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { imageFileFilter } from './helpers/imageFilter.helper';
@@ -13,6 +17,15 @@ import { imageRenamer } from './helpers/imageRenamer.helper';
 @Controller('files')
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
+
+  @Get('product/:imageName')
+  getProductImage(
+    @Res() res: Response, // hacemos override de la response de nest para servir la image en lugar del absolute path
+    @Param('imageName') imageName: string,
+  ): void {
+    const path = this.filesService.getProductImage(imageName);
+    res.sendFile(path);
+  }
 
   //upload files
   @Post('product')
--- src/files/files.service.ts
+++ src/files/files.service.ts
@@ -1,4 +1,6 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
+import { existsSync } from 'fs';
+import { join } from 'path';
 
 @Injectable()
 export class FilesService {
@@ -7,9 +9,22 @@ export class FilesService {
       throw new BadRequestException('Are you sure file is a image?');
     }
 
+    const secureUrl = `${file.filename}`;
+
     // return Promise.resolve(undefined);
     return {
-      filename: file.originalname,
+      filename: secureUrl,
     };
+  }
+
+  getProductImage(imageName: string): string {
+    const path = join(__dirname, '../../../static/uploads/products', imageName); // path fisico fuera del serve static de nest
+    console.log(__dirname);
+    if (!existsSync(path))
+      throw new BadRequestException(
+        `Not product found with image ${imageName}`,
+      );
+
+    return path;
   }
 }
