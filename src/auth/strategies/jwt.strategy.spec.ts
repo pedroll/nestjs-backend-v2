e0232bdec00c5202f1e1e73466dc5659c4873707
Pedro LLongo <pedrollongo@gmail.com>
2025-04-19 00:13:21 +0200

    test strategy, repository method

--- src/auth/strategies/jwt.strategy.spec.ts
+++ src/auth/strategies/jwt.strategy.spec.ts
@@ -4,12 +4,13 @@ import { User } from '../entities/user.entity';
 import { Repository } from 'typeorm';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
+import { JwtPayload } from '../interfaces';
+import { UnauthorizedException } from '@nestjs/common';
 
 describe('JwtStrategy', () => {
   // Creamos mock testing module
   let strategy: JwtStrategy;
   let userRepository: Repository<User>; // Simulamos el repositorio de usuarios
-  let jwtService: JwtStrategy; // Simulamos el servicio de JWT
 
   beforeEach(async () => {
     const mockUserRepository = {
@@ -35,11 +36,46 @@ describe('JwtStrategy', () => {
     }).compile();
 
     strategy = module.get<JwtStrategy>(JwtStrategy);
-    // userRepository = module.get(getRepositoryToken(User));
+    userRepository = module.get<Repository<User>>(getRepositoryToken(User));
     // jwtService = module.get(JwtStrategy); // Simulamos el servicio de JWT
   });
 
   it('should be defined', () => {
     expect(strategy).toBeDefined();
+  });
+
+  it('should validate and return a user if exists and active', async () => {
+    // simulamos el flujo de jwtStrategy.validate
+    const payload: JwtPayload = { id: 'some-uuid' };
+    const mockUser = { id: 'some-uuid', isActive: true } as User;
+
+    jest.spyOn(userRepository, 'findOne').mockResolvedValue(mockUser); // Simulamos findOne
+    const result = await strategy.validate(payload);
+
+    expect(result).toEqual(mockUser);
+  });
+
+  it('should Throw an exception if the user is not found', async () => {
+    // simulamos el flujo de jwtStrategy.validate
+    const payload: JwtPayload = { id: 'some-uuid' };
+
+    jest.spyOn(userRepository, 'findOne').mockResolvedValue(null); // Simulamos findOne
+
+    await expect(strategy.validate(payload)).rejects.toThrow(
+      UnauthorizedException,
+    );
+    await expect(strategy.validate(payload)).rejects.toThrow('Token no valido');
+  });
+
+  it('should Throw an exception if the user is inactive', async () => {
+    // simulamos el flujo de jwtStrategy.validate
+    const payload: JwtPayload = { id: 'some-uuid' };
+    const mockUser = { id: 'some-uuid', isActive: false } as User;
+
+    jest.spyOn(userRepository, 'findOne').mockResolvedValue(mockUser); // Simulamos findOne
+    const result = strategy.validate(payload);
+
+    await expect(result).rejects.toThrow(UnauthorizedException);
+    await expect(result).rejects.toThrow('Usuario inactivo');
   });
 });
