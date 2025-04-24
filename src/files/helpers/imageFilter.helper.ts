dd1a2a3018e9c903f14754dbf58936fb6732b755
Pedro LLongo <pedrollongo@gmail.com>
2025-04-25 00:24:17 +0200

    e2e: file-upload

--- src/files/helpers/imageFilter.helper.ts
+++ src/files/helpers/imageFilter.helper.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { BadRequestException, Logger } from '@nestjs/common';
 
 const logger = new Logger('ImageFileFilter');
 
@@ -7,15 +7,22 @@ export const imageFileFilter = (
   file: Express.Multer.File,
   callback: (error: Error | null, acceptFile: boolean) => void,
 ) => {
-  if (!file) return callback(new Error('there is no file'), false);
+  if (!file)
+    return callback(new BadRequestException('there is no file'), false);
   if (!file.mimetype.includes('image')) {
-    return callback(new Error('Only image files are allowed!'), false);
+    return callback(
+      new BadRequestException('Only image files are allowed!'),
+      false,
+    );
   }
   const allowedTypes = ['image/jpeg', 'image/png', 'image/jpg', 'image/gif'];
 
   if (!allowedTypes.includes(file.mimetype)) {
     logger.error('Only jpeg, png, jpg, gif extensions are allowed!');
-    return callback(new Error('Only image files are allowed!'), false);
+    return callback(
+      new BadRequestException('Only image files are allowed!'),
+      false,
+    );
   }
 
   logger.log({ file });
--- test/e2e/files/file-upload.e2e-spec.ts
+++ test/e2e/files/file-upload.e2e-spec.ts
@@ -3,23 +3,15 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import * as request from 'supertest';
 import { App } from 'supertest/types';
+import { join } from 'path';
+import { existsSync, unlinkSync } from 'fs';
+
 import { AppModule } from '../../../src/app.module';
-import { User } from '../../../src/auth/entities/user.entity';
-
-interface ResponseBody {
-  message?: string[];
-  error?: string;
-  statusCode?: number;
-  user?: User;
-  token?: string;
-}
-
-function delay(ms: number) {
-  return new Promise<void>((resolve) => setTimeout(resolve, ms));
-}
 
 describe('AuthModule Private (e2e)', () => {
   let app: INestApplication<App>;
+
+  const testImagePath = join(__dirname, 'nestjs.png');
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -47,12 +39,63 @@ describe('AuthModule Private (e2e)', () => {
     await app.close();
   });
 
-  it('should throw error if no file selected', async () => {
+  it('should throw 400 error if no file selected', async () => {
     const response = await request(app.getHttpServer()).post('/files/product');
 
     expect(response.status).toBe(400);
     expect(response.body).toEqual({
       message: 'Make sure that the file is an image',
+      error: 'Bad Request',
+      statusCode: 400,
+    });
+  });
+
+  it("should throw 400 error if file selected isn't an image", async () => {
+    const response = await request(app.getHttpServer())
+      .post('/files/product')
+      .attach('file', Buffer.from('not an image'), 'not-an-image.txt');
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      message: 'Only image files are allowed!',
+      error: 'Bad Request',
+      statusCode: 400,
+    });
+  });
+
+  it('should upload image successfully', async () => {
+    const response = await request(app.getHttpServer())
+      .post('/files/product')
+      .attach('file', testImagePath, 'nestjs.png');
+
+    expect(response.status).toBe(201);
+    expect(response.body).toHaveProperty('secureUrl');
+    expect(response.body).toHaveProperty('fileName');
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    expect(response.body.secureUrl).toContain('/files/product');
+
+    const filePath = join(
+      __dirname,
+      '../../../',
+      'static',
+      'uploads',
+      'products',
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-argument,@typescript-eslint/no-unsafe-member-access
+      response.body.fileName,
+    );
+    const fileExists = existsSync(filePath);
+    expect(fileExists).toBeTruthy();
+    unlinkSync(filePath);
+  });
+
+  it('should throw error if requested file not found', async () => {
+    const response = await request(app.getHttpServer()).get(
+      '/files/product/image-not-exists.jpg',
+    );
+
+    expect(response.status).toBe(400);
+    expect(response.body).toEqual({
+      message: 'Not product found with image image-not-exists.jpg',
       error: 'Bad Request',
       statusCode: 400,
     });
