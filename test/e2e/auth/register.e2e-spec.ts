f5cdd1750f8f03d75c3d5d7a21ccac0fa9269e00
Pedro LLongo <pedrollongo@gmail.com>
2025-04-24 17:15:09 +0200

    e2e: register

--- test/e2e/auth/register.e2e-spec.ts
+++ test/e2e/auth/register.e2e-spec.ts
@@ -6,7 +6,7 @@ import * as request from 'supertest';
 import { App } from 'supertest/types';
 
 import { AppModule } from '../../../src/app.module';
-import { CreateUserDto, LoginUserDto } from '../../../src/auth/dto';
+import { CreateUserDto } from '../../../src/auth/dto';
 import { User } from '../../../src/auth/entities/user.entity';
 import { getRepositoryToken } from '@nestjs/typeorm';
 
@@ -16,19 +16,19 @@ interface ResponseBody {
   statusCode?: number;
 }
 
-const testingUser = {
-  email: 'testing.user@gmail.com',
+const testingExistingUser = {
+  email: 'testing.existing@gmail.com',
   password: '123456Aa$',
   fullName: 'Testing User',
 } as CreateUserDto;
 
-const testingAdminUser = {
-  email: 'testing.admin@gmail.com',
+const testingRegisterUser = {
+  email: 'testing.register@gmail.com',
   password: '123456Aa$',
   fullName: 'Testing Admin',
 } as CreateUserDto;
 
-describe('Login (e2e)', () => {
+describe('Register (e2e)', () => {
   let app: INestApplication<App>;
   let userRepository: Repository<User>;
 
@@ -53,28 +53,23 @@ describe('Login (e2e)', () => {
 
     await app.init();
 
-    await request(app.getHttpServer()).post('/auth/register').send(testingUser);
     await request(app.getHttpServer())
       .post('/auth/register')
-      .send(testingAdminUser);
-
-    // add testing users
-    userRepository = app.get<Repository<User>>(getRepositoryToken(User));
-    await userRepository.update(
-      { email: testingAdminUser.email },
-      { roles: ['admin'] },
-    );
+      .send(testingExistingUser);
   });
 
   afterEach(async () => {
     // delete testing users
-    await userRepository.delete({ email: testingUser.email });
-    await userRepository.delete({ email: testingAdminUser.email });
+    userRepository = app.get<Repository<User>>(getRepositoryToken(User));
+
+    await userRepository.delete({ email: testingExistingUser.email });
+    await userRepository.delete({ email: testingRegisterUser.email });
 
     await app.close();
   });
 
-  it('/auth/login (POST) - no body should throw 400 Bad Request', async () => {
+  it('/auth/register (POST) - no body should throw 400 Bad Request', async () => {
+    // Evaluar errores esperados
     const errorMessages = [
       'email should not be empty',
       'email must be shorter than or equal to 50 characters',
@@ -85,13 +80,13 @@ describe('Login (e2e)', () => {
       'password must be shorter than or equal to 50 characters',
       'password must be longer than or equal to 6 characters',
       'password must be a string',
+      'fullName must be shorter than or equal to 50 characters',
+      'fullName must be longer than or equal to 3 characters',
+      'fullName must be a string',
     ];
 
-    const response = await request(app.getHttpServer()).post('/auth/login');
-
+    const response = await request(app.getHttpServer()).post('/auth/register');
     const body = response.body as ResponseBody;
-
-    // console.log(response.body);
 
     expect(response.status).toBe(400);
     errorMessages.forEach((error) => {
@@ -99,65 +94,70 @@ describe('Login (e2e)', () => {
     });
   });
 
-  it('/auth/login (POST) - wrong credentials email', async () => {
+  it('/auth/register (POST) - same email', async () => {
     const dto = {
-      email: 'wrongemail@example.com',
-      password: testingUser.password,
-    } as LoginUserDto;
+      ...testingRegisterUser,
+      email: testingExistingUser.email,
+    } as CreateUserDto;
 
     const response = await request(app.getHttpServer())
-      .post('/auth/login')
+      .post('/auth/register')
       .send(dto);
     const body = response.body as ResponseBody;
 
-    expect(response.status).toBe(401);
+    expect(response.status).toBe(400);
     expect(body).toEqual({
-      message: 'Invalid credentials email',
-      error: 'Unauthorized',
-      statusCode: 401,
+      message: `Key (email)=(${testingExistingUser.email}) already exists.`,
+      error: 'Bad Request',
+      statusCode: 400,
     });
   });
 
-  it('/auth/login (POST) - wrong credentials password', async () => {
+  it('/auth/register (POST) - unsafe password', async () => {
     const dto = {
-      email: testingUser.email,
-      password: 'Wrongpassword1!',
-    } as LoginUserDto;
+      ...testingRegisterUser,
+      password: 'wrongpassword',
+    } as CreateUserDto;
 
     const response = await request(app.getHttpServer())
-      .post('/auth/login')
+      .post('/auth/register')
       .send(dto);
     const body = response.body as ResponseBody;
 
-    expect(response.status).toBe(401);
+    expect(response.status).toBe(400);
     expect(body).toEqual({
-      message: 'Invalid credentials password',
-      error: 'Unauthorized',
-      statusCode: 401,
+      message: [
+        'The password must have a Uppercase, lowercase letter and a number (?=.*\\d)(?:(?=.*\\W+))?(?![.\\n])(?=.*[A-Z])(?=.*[a-z]).*$',
+      ],
+      error: 'Bad Request',
+      statusCode: 400,
     });
   });
 
-  it('/auth/login (POST) - valid credentials', async () => {
+  it('/auth/register (POST) - valid credentials', async () => {
     const dto = {
-      email: testingUser.email,
-      password: testingUser.password,
-    } as LoginUserDto;
+      ...testingRegisterUser,
+    } as CreateUserDto;
 
     const response = await request(app.getHttpServer())
-      .post('/auth/login')
+      .post('/auth/register')
       .send(dto);
     const body = response.body as ResponseBody;
 
     expect(response.status).toBe(201);
     expect(body).toEqual({
       user: {
+        email: testingRegisterUser.email,
+        fullName: testingRegisterUser.fullName,
+        createdAt: expect.any(String) as string,
+        updatedAt: null,
         id: expect.any(String) as string,
-        email: testingUser.email,
-        fullName: testingUser.fullName,
         isActive: true,
         roles: ['user'],
       },
       token: expect.any(String) as string,
     });
   });
+
+  it.todo('/auth/register (POST) - faltan parametros');
 });
