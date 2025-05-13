2e97a57503959751d358cc7a1209bbf5b93a6a55
Pedro LLongo <pedrollongo@gmail.com>
2025-05-14 01:09:42 +0200

    orthography check use-case

--- src/gpt/gpt.service.ts
+++ src/gpt/gpt.service.ts
@@ -1,10 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { OrthographyDto } from './dto';
 import { orthographyUseCase } from './use-case';
+import OpenAI from 'openai';
+import * as process from 'node:process';
 
 @Injectable()
 export class GptService {
+  private openAi = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+  });
+
   async orthographyCheck(orthographyDto: OrthographyDto) {
-    return await orthographyUseCase(orthographyDto);
+    return await orthographyUseCase(this.openAi, orthographyDto);
   }
 }
--- src/gpt/use-case/ortography-use-case/ortography.use-case.ts
+++ src/gpt/use-case/ortography-use-case/ortography.use-case.ts
@@ -1,12 +1,45 @@
+import OpenAI from 'openai';
+
 interface Options {
   prompt: string;
   maxTokens?: number;
 }
 
-export const orthographyUseCase = async (options: Options) => {
-  const { prompt } = options;
-  console.log({ prompt });
-  return {
-    prompt,
-  };
+export const orthographyUseCase = async (openAi: OpenAI, options: Options) => {
+  const { prompt, maxTokens } = options;
+
+  const response = await openAi.responses.create({
+    model: 'gpt-4.1-nano',
+    input: [
+      {
+        role: 'system',
+        content: `Te serán  proveídos textos en español con posible errores ortográficos y gramaticales,
+          Las palabras deben existir en el diccionario de la Real Academia Española
+          Debes responder en formato JSON,
+          tu tarea es corregirlos y retornar información de soluciones,
+          tambien debes dar ub porcentaje de acierto para el usuario.
+          
+          Si no hay errores felicita al usuario.
+          
+          Ejemplo de salida:
+          {
+          userScore: number,
+          errors: string[] // ['error -> solución'],
+          message: string, // usa emojis y texto para felicitar al usuario
+          }`,
+      },
+      {
+        role: 'user',
+        content: prompt,
+      },
+    ],
+    max_output_tokens: maxTokens,
+    temperature: 0.2,
+    // response_format: {
+    //   type: 'json_object'
+    // },
+  });
+  // console.log(response);
+  const jsonResponse = JSON.parse(response.output_text) as JSON;
+  return jsonResponse;
 };
