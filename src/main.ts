8f4571766c49665ebe264e9e39639028e848f3c1
Pedro LLongo <pedrollongo@gmail.com>
2025-05-15 23:24:46 +0200

    backend cors options

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+const corsOptions: CorsOptions = {
+  origin: ['http://localhost:4200'], // Allow requests from Angular frontend
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+};
 
 export async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +19,7 @@ export async function bootstrap() {
     new ValidationPipe({
       whitelist: true, // elimina los atributos que no esten en el dto
       forbidNonWhitelisted: true, // devuelve error si hay atributos que no esten en el dto
+      forbidUnknownValues: true,
       // transform: true, // transforma los datos a los tipos que se especifican en el dto
       // transformOptions: {
       //   enableImplicitConversion: true,
@@ -28,7 +37,7 @@ export async function bootstrap() {
   const documentFactory = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
 
-  app.enableCors();
+  app.enableCors(corsOptions);
   //app.enableCors(options);
 
   const port = process.env.PORT ?? 3000;
